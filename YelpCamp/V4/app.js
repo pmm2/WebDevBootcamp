f1666bbdfae71c7c7f3c7637d923814867373438
pmm <pmm@cin.ufpe.br>
2020-07-08 16:19:25 -0300

    V4

--- YelpCamp/V4/app.js
+++ YelpCamp/V4/app.js
@@ -4,7 +4,8 @@ bodyParser = require("body-parser");
 request = require("request");
 mongoose = require("mongoose");
 Campground = require("./models/campground");
-seedDB =require("./seeds.js")
+Comment = require("./models/comment");
+seedDB =require("./seeds.js");
 app.use(bodyParser.urlencoded({ extended: true }));
 seedDB();
 mongoose.connect("mongodb://localhost:27017/YelpCamp", {
@@ -14,7 +15,7 @@ mongoose.connect("mongodb://localhost:27017/YelpCamp", {
 //Schemas
 
 app.get("/", function (req, res) {
-  res.render("home.ejs");
+  res.render("campgrounds/home.ejs");
 });
 
 app.get("/campgrounds", function (req, res) {
@@ -23,7 +24,7 @@ app.get("/campgrounds", function (req, res) {
     if (err) {
       console.log("Deu errado");
     } else {
-      res.render("index.ejs", { campgrounds: object });
+      res.render("campgrounds/index.ejs", { campgrounds: object });
     }
   });
 });
@@ -52,7 +53,7 @@ app.post("/campgrounds", function (req, res) {
 
 //Mostra form para criar campground
 app.get("/campgrounds/new", function (req, res) {
-  res.render("new.ejs");
+  res.render("campgrounds/new.ejs");
 });
 
 app.get("/campgrounds/:id",function (req,res) {
@@ -61,8 +62,40 @@ app.get("/campgrounds/:id",function (req,res) {
       console.log("Deu errado")
     }else{
       console.log(object);
-      res.render("show.ejs",{campground:object});
+      res.render("campgrounds/show.ejs",{campground:object});
     }
   })
   
 });
+//Comentarios
+
+//Procura o campgrounds na base de dados e cria um form para um comentario nele
+app.get("/campgrounds/:id/comments/new", function (req, res) {
+  Campground.findById(req.params.id, function (err, campground) {
+    if(err){
+      console.log("erro");
+    }else{
+      console.log(campground);
+      res.render("comments/new.ejs",{campground: campground});
+    }
+  });
+});
+//Procura um campground e atribui um comentario feito no form anterior
+app.post("/campgrounds/:id/comments", function (req, res) {
+  Campground.findById(req.params.id, function (err, campground) {
+    if(err){
+      console.log("erro");
+    }else{
+     Comment.create(req.body.comment,function (err,comment) {
+      if(err){
+        console.log("erro");
+      }else{
+        campground.comments.push(comment);
+        campground.save();
+        res.redirect("/campgrounds/"+campground._id);
+      }
+     })
+    }
+  });
+});
+
